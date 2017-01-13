fff252fab22373a2ee9b3e52285b2ed8848739e9
Eldos <eldosnarbay@gmail.com>
2017-01-14 01:36:03 +0600

    Procedural style. draw() function

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,6 +10,15 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            draw(1, 3, '*');
+            draw(2, 4, '#');
+            Console.ReadLine();
+        }
+
+        static void draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
